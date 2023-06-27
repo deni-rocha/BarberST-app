ddda7e422d74128fa87ae22e3ee5d7ebbf41cc8c
Denilson Rocha <denilsonrocha_@outlook.com>
2023-06-27 16:25:07 -0300

    constants de rotas

---
+++ src/constants/app-routes.ts
@@ -0,0 +1,16 @@
+export const APP_ROUTES = {
+  private: {
+    user: {
+      home: "/user/home",
+    },
+    admin: {
+      home: "/admin/home",
+      register: "/admin/register",
+    },
+  },
+  public: {
+    userLogin: "/user/login",
+    userRegister: "/user/register",
+    adminLogin: "/admin/login",
+  },
+};
